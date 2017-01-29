43ed4ba48049c9d892cd9ab3275f02a456caea54
Jamieson Bates <jamiesonbates@gmail.com>
2017-01-28 21:34:56 -0800

    Add filter and sort functionality

--- app.js
+++ app.js
@@ -12,9 +12,49 @@
           title: 'Darts are Awesome',
           body: 'Darts are so awesome that I play at least 15 games a day. One at home and 14 at the bar.',
           author: 'Barstool Sports',
+          votes: 2
+       },
+        {
+          image: 'https://3hsyn13u3q9dhgyrg2qh3tin-wpengine.netdna-ssl.com/wp-content/uploads/2017/01/SplitShire-8418-1024x682.jpg',
+          title: 'Man sits alone in pub',
+          body: 'But he is not alone, he is surrounded by a few of his favorite things',
+          author: 'Bar Times',
           votes: 0
-       }
+       },
+        {
+          image: 'https://3hsyn13u3q9dhgyrg2qh3tin-wpengine.netdna-ssl.com/wp-content/uploads/2017/01/SplitShire-0424-1024x768.jpg',
+          title: 'Paraglide your way to paradise',
+          body: 'From mountain top to valley bottom, you will spend amazing moments in the air above an incredibly vibrant forest.',
+          author: 'Outdoor Mag',
+          votes: 5
+       },
+       {
+         image: 'https://3hsyn13u3q9dhgyrg2qh3tin-wpengine.netdna-ssl.com/wp-content/uploads/2017/01/SplitShire-3600.jpg',
+         title: 'Skiing Changes You',
+         body: 'Get out there this winter. Skiing is an excistential experience where you you experience speed, agility, and utter remoteness all in one.',
+         author: 'Skiing News',
+         votes: 7
+      }];
+
+      vm.options = [
+        {
+          id: 1,
+          label: 'Sort by votes',
+          orderBy: `-votes`
+        },
+        {
+          id: 2,
+          label: 'Sort by date',
+          orderBy: `date`
+        },
+        {
+          id: 3,
+          label: 'Sort by title',
+          orderBy: `title`
+        }
       ];
+
+      vm.selected = vm.options[0];
     }
 
     vm.showPost = function() {
@@ -46,6 +86,10 @@
         }
       });
     }
+
+    vm.searchByTitle = function() {
+      console.log(vm.search);
+    }
   }
 
   angular.module('app', [])
@@ -55,21 +99,18 @@
         <main>
           <section class="filters">
             <div class="filter-container">
-              <input class="text-filter type="text">
-              <select class="select-filter">
-                <option>Sort by Votes</option>
-                <option>Sort by Date</option>
-                <option>Sort by Title</option>
+              <input class="text-filter type="text" ng-model="$ctrl.search" ng-change="$ctrl.searchByTitle()">
+              <select class="select-filter" ng-options="option as option.label for option in $ctrl.options track by option.id" ng-model="$ctrl.selected">
               </select>
             </div>
           </section>
 
           <section ng-style={{ $ctrl.contentHeight }}>
-            <div class="post" ng-repeat="post in $ctrl.posts">
+            <div class="post" ng-repeat="post in $ctrl.posts | filter:$ctrl.search | orderBy:$ctrl.selected.orderBy">
               <div class="votes">
-                <i class="material-icons" ng-click="$ctrl.updateVotes(post, 'up')">thumb_up</i>
+                <i class="material-icons" ng-click="$ctrl.updateVotes(post, 'up')">keyboard_arrow_up</i>
                 <p>{{post.votes}}</p>
-                <i class="material-icons" ng-click="$ctrl.updateVotes(post, 'down')">thumb_down</i>
+                <i class="material-icons" ng-click="$ctrl.updateVotes(post, 'down')">keyboard_arrow_down</i>
               </div>
 
               <div class="image">
@@ -133,7 +174,7 @@
                 <input
                   type="text"
                   name="image"
-                  ng-model="$ctrl.post.image">
+                  ng-model="$ctrl.post">
               </div>
 
               <div class="form-submit">
