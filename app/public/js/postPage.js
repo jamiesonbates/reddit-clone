f4b63769aefad7ae9833836c882d8a15638ed29b
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-01 08:48:28 -0800

    Style post page and get comments and votes in there

--- app/public/js/postPage.js
+++ app/public/js/postPage.js
@@ -8,28 +8,57 @@
         <main>
           <section class="single-post">
             <form ng-submit="$ctrl.updatePost()">
-              <div>
+              <div class="image">
                 <img src="{{ $ctrl.post.image_url }}">
                 <a href="#" ng-click="$ctrl.editImage()">Edit Image</a>
                 <input type="text" ng-model="$ctrl.post.image_url" ng-if="$ctrl.editingImage">
               </div>
-              <div class="details">
-                <div class="opening">
-                  <h3>{{ $ctrl.post.title}}</h3>
-                  <a href="#" ng-click="$ctrl.editTitle()">Edit Title</a>
+              <div class="post-details">
+                <div class="post-title">
+                  <h3 ng-show="$ctrl.editingTitle===false">{{ $ctrl.post.title}}</h3>
                   <input type="text" ng-model="$ctrl.post.title" ng-if="$ctrl.editingTitle">
-
-                  <h4>{{ $ctrl.post.author }}</h4>
-                  <a href="#" ng-click="$ctrl.editAuthor()">Edit Author</a>
-                  <input type="text" ng-model="$ctrl.post.author" ng-if="$ctrl.editingAuthor">
+                  <a href="#" ng-click="$ctrl.editTitle()">(edit)</a>
                 </div>
-                <p>{{ $ctrl.post.body }}</p>
-                <a href="#" ng-click="$ctrl.editBody()">Edit Body</a>
-                <input tyoe="text" ng-model="$ctrl.post.body" ng-if="$ctrl.editingBody">
+
+                <div class="post-author">
+                  <h4 ng-show="$ctrl.editingAuthor===false">{{ $ctrl.post.author }}</h4>
+                  <input type="text" ng-model="$ctrl.post.author" ng-if="$ctrl.editingAuthor">
+                  <a href="#" ng-click="$ctrl.editAuthor()">(edit)</a>
+                </div>
+
+                <div class="post-body">
+                  <p ng-show="$ctrl.editingBody===false">{{ $ctrl.post.body }}</p>
+                  <input type="text" ng-model="$ctrl.post.body" ng-if="$ctrl.editingBody">
+                  <a href="#" ng-click="$ctrl.editBody()">(edit)</a>
+                </div>
               </div>
 
               <button type="submit" ng-if="$ctrl.editingImage || $ctrl.editingTitle || $ctrl.editingAuthor || $ctrl.editingBody">Submit</button>
             </form>
+
+            <div class="post-comments">
+              <div class="comment-meta">
+                <i class="material-icons">comment</i>
+                <h3>Comments</h3>
+              </div>
+
+              <div class="post-comments-form">
+                <form ng-submit="$ctrl.addComment(post)">
+                  <input ng-model="$ctrl.newComment" placeholder="Write Comment">
+                  <button type="submit">Comment</button>
+                </form>
+
+                <div ng-repeat="comment in post.allComments">
+                  <p>{{ comment.content }}</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="post-votes">
+              <i class="material-icons" ng-click="$ctrl.updateVotes(post, 'up')">keyboard_arrow_up</i>
+              <p>{{post.vote_count}}</p>
+              <i class="material-icons" ng-click="$ctrl.updateVotes(post, 'down')">keyboard_arrow_down</i>
+            </div>
           </section>
         </main>
       `
